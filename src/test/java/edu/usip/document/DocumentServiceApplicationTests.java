3af84f0dca07c43f2c75bcb79c7f7cd0c4d4b1c3
Alejandro Salazar <asalazarm@pilandina.com.bo>
2026-03-08 03:24:45 -0400

    first commit

---
+++ src/test/java/edu/usip/document/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usip.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
